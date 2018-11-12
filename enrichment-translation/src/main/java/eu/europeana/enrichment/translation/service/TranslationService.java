14d6522bef682e7d521b3d208e3ebaa2553132c9
Denis Katic <denis.katic@ait.ac.at>
2018-11-12 16:01:09 +0100

    enrichment-translation:
     - Google cloud translation added
     - JUnit for Google translation added

---
+++ enrichment-translation/src/main/java/eu/europeana/enrichment/translation/service/TranslationService.java
@@ -0,0 +1,19 @@
+package eu.europeana.enrichment.translation.service;
+
+import eu.europeana.enrichment.translation.exception.TranslationException;
+
+
+public interface TranslationService {
+	
+	public static final String HANDLER_ANNOTATE = "/translate";
+	
+	/**
+	 * This method identifies named entities.
+	 * 
+	 * @param text is the transcribed text
+	 * @param sourceLanguage is the language of the transcribed text
+	 * @return the translated text
+	 * @throws TranslationException	
+	 */
+	public String translateText(String text, String sourceLanguage) throws TranslationException;
+}
---
+++ enrichment-translation/src/test/java/eu/europeana/enrichment/translation/service/TranslationGoogleServiceTest.java
@@ -0,0 +1,56 @@
+package eu.europeana.enrichment.translation.service;
+
+import static org.junit.Assert.*;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+
+import eu.europeana.enrichment.translation.service.impl.TranslationGoogleServiceImpl;
+
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration({ "/entity-solr-context.xml" })
+public class TranslationGoogleServiceTest {
+
+	@Resource
+	TranslationGoogleServiceImpl translationGoogleService;
+	
+	private static final String testString = "Mein Großvater, Joseph (Arthur Maria) Kister, geb. 25.9.1887 in Essen, " +
+			"diente während des 1. Weltkriegs zunächst in der Infanterie in Frankreich. " +
+			"Nach eigener Aussage war ihm das „zu schmutzig“ und er bewarb sich daher zur Luftwaffe. Seine Einsätze hatte er ab Juni " +
+			"1918 von St. Remy aus. Nur aus dieser Zeit gibt es Tagebuch-Aufzeichungen von ihm mit " +
+			"lebhaften Beschreibungen von Luftkämpfen. Er wurde am 22.8.1918 bei einem Luftkampf, den er detailliert beschreibt, " +
+			"verwundet und kam zurück nach Deutschland in ein Lazarett. Ab 1.12.18 war er wieder in Essen als Kanzleigehilfe. " +
+			"Später zog er nach Burgsteinfurt im Münsterland, wo er Obergerichtsvollzieher war. Er starb am 10.10.1979. " +
+			"Zeit seines Lebens hingen Erinnerungen an seine Zeit als Jagdflieger in seinem Wohnzimmer. " +
+			"Er war stets sehr an Technik interessiert und fotografierte viel. Aus den Jahren 1916-1918 sind ca 100 Fotoplatten " +
+			"und Rollfilm-Negative erhalten, einige mit genauen Angaben über Belichtungszeit, Tageszeit und Lichtverhältnisse.";
+	
+	@Test
+	public void testTranslationGoogleImplementation() {
+		
+		String expectedTranslation = "My grandfather, Joseph (Arthur Maria) Kister, b. 25.9.1887 in Essen, " + 
+				"served during the 1st World War first in the infantry in France. By his own admission, this was \"too dirty\" for him " + 
+				"and he therefore applied to the Air Force. He had his missions from June 1918 from St. Remy. " + 
+				"Only from this time there are diary records of him with vivid descriptions of aerial combat. " +
+				"He was wounded on 22.8.1918 in a dogfight, which he describes in detail, and came back to Germany in a military hospital. " + 
+				"From 1.12.18 he was back in Essen as an office assistant. Later he moved to Burgsteinfurt in the Münsterland, " +
+				"where he was chief justice. He died on 10.10.1979. Throughout his life, memories of his time as a " +
+				"fighter pilot hung in his living room. He was always very interested in technology and photographed a lot. " +
+				"From the years 1916-1918 about 100 photo plates and roll film negatives have been preserved, some with exact " + 
+				"information about exposure time, time of day and light conditions.";
+		
+		//String jsonPath = "C:\\Users\\katicd\\Documents\\Europeana\\Code\\Ait\\google-cloud\\EU-Europeana-enrichment-d92edee4115a.json";
+		String jsonPath = "";
+		int size = expectedTranslation.length();
+		
+		translationGoogleService = new TranslationGoogleServiceImpl(jsonPath);
+		String translatedText = translationGoogleService.translateText(testString, "de");
+		System.out.println("Translated text: " + translatedText);
+		assertNotNull(translatedText);
+		assertEquals(expectedTranslation, translatedText);
+		
+	}
+
+}
