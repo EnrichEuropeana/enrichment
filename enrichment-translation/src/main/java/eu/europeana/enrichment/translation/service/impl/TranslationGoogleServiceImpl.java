947e96eb951186456af100fd7a3505e9da015636
Denis Katic <denis.katic@ait.ac.at>
2018-11-12 16:01:09 +0100

    enrichment-translation:
     - Google cloud translation added
     - JUnit for Google translation added

---
+++ enrichment-translation/src/main/java/eu/europeana/enrichment/translation/service/impl/TranslationGoogleServiceImpl.java
@@ -0,0 +1,60 @@
+package eu.europeana.enrichment.translation.service.impl;
+
+import java.io.FileInputStream;
+
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+import com.google.common.collect.Lists;
+import com.google.cloud.translate.Translate.TranslateOption;
+
+
+import com.google.api.gax.paging.Page;
+//import com.google.auth.appengine.AppEngineCredentials;
+import com.google.auth.oauth2.ComputeEngineCredentials;
+import com.google.auth.oauth2.GoogleCredentials;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import eu.europeana.enrichment.translation.exception.TranslationException;
+import eu.europeana.enrichment.translation.service.TranslationService;
+
+//https://cloud.google.com/translate/docs/reference/libraries
+public class TranslationGoogleServiceImpl implements TranslationService{
+
+	// Instantiates a client
+    Translate translate;
+
+    public TranslationGoogleServiceImpl(String jsonPath) {
+    	try {
+    		// You can specify a credential file by providing a path to GoogleCredentials.
+    		// Otherwise credentials are read from the GOOGLE_APPLICATION_CREDENTIALS environment variable.
+    		GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(jsonPath)).createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
+        	translate = TranslateOptions.newBuilder().setCredentials(credentials).build().getService();
+    	}
+    	catch (Exception e) {
+			// TODO: handle exception
+    		System.err.println(e.getMessage());
+		}
+    	
+    }
+
+	
+	@Override
+	public String translateText(String text, String sourceLanguage) throws TranslationException {
+		
+		if(translate == null) {
+			//TODO: throws exception
+			return null;
+		}
+		
+	    Translation translation = translate.translate(text, 
+	    		TranslateOption.sourceLanguage(sourceLanguage), 
+	    		TranslateOption.targetLanguage("en"));
+
+		return translation.getTranslatedText();
+	}
+	
+	
+}
