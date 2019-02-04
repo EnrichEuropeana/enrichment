2f1eee032f1040672a81efb251bbfc981d827682
Denis Katic <denis.katic@ait.ac.at>
2019-02-04 17:21:16 +0100

    enrichment-ner: python scripts added (nltk, spaCy and flair)

--- enrichment-ner/src/main/java/eu/europeana/enrichment/ner/linking/WikidataServiceImpl.java
+++ enrichment-ner/src/main/java/eu/europeana/enrichment/ner/linking/WikidataServiceImpl.java
@@ -12,6 +12,7 @@ import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 public class WikidataServiceImpl implements WikidataService {
