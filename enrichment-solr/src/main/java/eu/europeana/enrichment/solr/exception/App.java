4f5992d22e24b2ea58eb17fbfa2fb9138a4eadf1
SrdjanStevanetic <srdjan.stevanetic@gmail.com>
2019-02-26 17:19:20 +0100

    started integrating Solr framework into enrichment projects

---
+++ enrichment-solr/src/main/java/eu/europeana/enrichment/solr/exception/App.java
@@ -0,0 +1,13 @@
+package eu.europeana.enrichment.solr.exception;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
