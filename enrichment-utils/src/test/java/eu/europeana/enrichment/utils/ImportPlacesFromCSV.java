2626e5f4dff5e78e3e93a3752b40406465954fc0
SrdjanStevanetic <srdjan.stevanetic@gmail.com>
2019-09-10 18:07:17 +0200

    1) JUnit tests for importing the Wikidata places from .csv files to the Wikidata .json and then from .json to .xml Solr documents; 2) Solr configurations for DataImportHandler for importing .xml documents to Solr

--- enrichment-utils/src/test/java/eu/europeana/enrichment/utils/ImportPlacesFromCSV.java
+++ enrichment-utils/src/test/java/eu/europeana/enrichment/utils/ImportPlacesFromCSV.java
@@ -1,129 +1,133 @@
-package eu.europeana.enrichment.utils;
-
-import static org.junit.Assert.assertTrue;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.Resource;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import eu.europeana.enrichment.common.commons.HelperFunctions;
-import eu.europeana.enrichment.model.WikidataEntity;
-import eu.europeana.enrichment.ner.linking.WikidataService;
-import eu.europeana.enrichment.solr.service.SolrWikidataEntityService;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:test-ner-config-book-dumitru.xml")
-
-/**
- * 
- * @author StevaneticS
- *
- * Importing stories and items to the mongo db from a json file
- */
-
-public class ImportPlacesFromCSV {
-	
-	@Resource(name = "wikidataService")
-	WikidataService wikidataService;
-	
-	@Resource
-	SolrWikidataEntityService solrWikidataEntityService;
-	
-	private String wikidataDirectory = "C:/wikidata";
-	
-	@Test
-	public void test() throws Exception {		
-
-		List<String> wikidataIDs = new ArrayList<String>();
-		
-		String [] pathName = new String [2];
-		pathName [0] = "C:/java/places_data_wikidata_unique.csv";
-		pathName [1] = "C:/java/places_data_wikidata_unique_with_label_description_v3.csv";
-		
-		String pathFileNotFoundEntities = "C:/java/not_found_wikidata_places.txt";
-		
-		for(int i=0; i<pathName.length;i++)
-		{
-			try (BufferedReader br = new BufferedReader(new FileReader(new File(pathName[i])))) {
-			    
-				String line;
-				List<String> labels = new ArrayList<String>();
-			    
-				while ((line = br.readLine()) != null) {
-			        
-					String[] values = line.split("\\s*,\\s*");	
-					
-					if(line.contains("\""))
-					{
-						for(int m=2;m<values.length;m++)
-						{
-							if(values[m].contains("www.wikidata.org")) 
-							{ 
-								wikidataIDs.add(values[m]);
-								break;
-							}
-						}
-						
-						int start_pos = line.indexOf("\"") + 1;
-						int end_pos = line.indexOf("\"", start_pos);
-						labels.add(line.substring(start_pos, end_pos));
-						
-					}
-					else
-					{
-						wikidataIDs.add(values[2]);
-						labels.add(values[1]);
-						
-					}
-					
-			    }
-				
-				for(int j=0;j<wikidataIDs.size();j++)
-				{
-					
-					boolean fileexists = HelperFunctions.checkWikidataJSONFileExistance(wikidataDirectory, wikidataIDs.get(j));
-					
-					if(fileexists==false) 	
-					{						
-						String WikidataJSON = wikidataService.getWikidataJSONFromWikidataID(wikidataIDs.get(j));
-						if(WikidataJSON==null || WikidataJSON.isEmpty()) 
-						{
-							try (BufferedWriter bw = new BufferedWriter(new FileWriter(new File(pathFileNotFoundEntities), true)))
-						    {					    	
-								bw.append(wikidataIDs.get(j)+ "," + labels.get(j));
-								    
-							} catch (IOException ioe) 
-						    {
-								ioe.printStackTrace();
-						    }
-
-						}
-						else
-						{
-							HelperFunctions.saveWikidataJsonToLocalFileCache(wikidataDirectory, wikidataIDs.get(j), WikidataJSON);
-							System.out.print("Analyzed wikidata entity number: " + j + " .\n");
-						}
-						
-					}
-
-					
-				}
-			}		
-		}
-		assertTrue(true);
-		
-	}
-
-}
+//package eu.europeana.enrichment.utils;
+//
+//import static org.junit.Assert.assertTrue;
+//
+//import java.io.BufferedReader;
+//import java.io.BufferedWriter;
+//import java.io.File;
+//import java.io.FileReader;
+//import java.io.FileWriter;
+//import java.io.IOException;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import javax.annotation.Resource;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//import eu.europeana.enrichment.common.commons.HelperFunctions;
+//import eu.europeana.enrichment.model.WikidataEntity;
+//import eu.europeana.enrichment.ner.linking.WikidataService;
+//import eu.europeana.enrichment.solr.service.SolrWikidataEntityService;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = "classpath:test-ner-config-book-dumitru.xml")
+//
+///**
+// * 
+// * @author StevaneticS
+// *
+// * Importing stories and items to the mongo db from a json file
+// */
+//
+//public class ImportPlacesFromCSV {
+//	
+//	@Resource(name = "wikidataService")
+//	WikidataService wikidataService;
+//	
+//	@Resource
+//	SolrWikidataEntityService solrWikidataEntityService;
+//	
+//	private String wikidataDirectory = "C:/wikidata";
+//	
+//	@Test
+//	public void test() throws Exception {		
+//
+//		List<String> wikidataIDs = new ArrayList<String>();
+//		
+//		String [] pathName = new String [2];
+//		pathName [0] = "C:/java/places_data_wikidata_unique.csv";
+//		pathName [1] = "C:/java/places_data_wikidata_unique_with_label_description_v3.csv";
+//		
+//		String pathFileNotFoundEntities = "C:/java/not_found_wikidata_places.txt";
+//		
+//		for(int i=0; i<pathName.length;i++)
+//		{
+//			try (BufferedReader br = new BufferedReader(new FileReader(new File(pathName[i])))) {
+//			    
+//				String line;
+//				List<String> labels = new ArrayList<String>();
+//			    
+//				while ((line = br.readLine()) != null) {
+//			        
+//					String[] values = line.split("\\s*,\\s*");	
+//					
+//					if(line.contains("\""))
+//					{
+//						for(int m=2;m<values.length;m++)
+//						{
+//							if(values[m].contains("www.wikidata.org")) 
+//							{ 
+//								wikidataIDs.add(values[m]);
+//								break;
+//							}
+//						}
+//						
+//						int start_pos = line.indexOf("\"") + 1;
+//						int end_pos = line.indexOf("\"", start_pos);
+//						labels.add(line.substring(start_pos, end_pos));
+//						
+//					}
+//					else
+//					{
+//						if(values[2].contains("www.wikidata.org"))
+//						{
+//							wikidataIDs.add(values[2]);
+//							labels.add(values[1]);
+//						}
+//						
+//					}
+//					
+//			    }
+//				
+//				for(int j=0;j<wikidataIDs.size();j++)
+//				{
+//					
+//					boolean fileexists = HelperFunctions.checkWikidataJSONFileExistance(wikidataDirectory, wikidataIDs.get(j));
+//					
+//					if(fileexists==false) 	
+//					{			
+//						System.out.print("Current wikidata id is: " + wikidataIDs.get(j) + ". Current index is: " + j + " .\n");
+//						String WikidataJSON = wikidataService.getWikidataJSONFromWikidataID(wikidataIDs.get(j));
+//						if(WikidataJSON==null || WikidataJSON.isEmpty()) 
+//						{
+//							try (BufferedWriter bw = new BufferedWriter(new FileWriter(new File(pathFileNotFoundEntities), true)))
+//						    {					    	
+//								bw.append(wikidataIDs.get(j)+ "," + labels.get(j));
+//								    
+//							} catch (IOException ioe) 
+//						    {
+//								ioe.printStackTrace();
+//						    }
+//
+//						}
+//						else
+//						{
+//							HelperFunctions.saveWikidataJsonToLocalFileCache(wikidataDirectory, wikidataIDs.get(j), WikidataJSON);
+//							System.out.print("Analyzed wikidata entity number: " + j + " .\n");
+//						}
+//						
+//					}
+//
+//					
+//				}
+//			}		
+//		}
+//		assertTrue(true);
+//		
+//	}
+//
+//}
--- enrichment-utils/src/test/java/eu/europeana/enrichment/utils/ImportPlacesFromCSVToSolr.java
+++ enrichment-utils/src/test/java/eu/europeana/enrichment/utils/ImportPlacesFromCSVToSolr.java
@@ -1,169 +1,206 @@
-package eu.europeana.enrichment.utils;
-
-import static org.junit.Assert.assertTrue;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-
-import javax.annotation.Resource;
-
-import org.apache.commons.io.FileUtils;
-import org.apache.solr.client.solrj.beans.DocumentObjectBinder;
-import org.apache.solr.client.solrj.util.ClientUtils;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import eu.europeana.enrichment.model.WikidataAgent;
-import eu.europeana.enrichment.model.WikidataEntity;
-import eu.europeana.enrichment.model.WikidataPlace;
-import eu.europeana.enrichment.ner.linking.WikidataService;
-import eu.europeana.enrichment.solr.model.SolrWikidataAgentImpl;
-import eu.europeana.enrichment.solr.model.SolrWikidataPlaceImpl;
-import eu.europeana.enrichment.solr.service.SolrWikidataEntityService;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:test-ner-config-book-dumitru.xml")
-
-/**
- * 
- * @author StevaneticS
- *
- * Importing stories and items to the mongo db from a json file
- */
-
-public class ImportPlacesFromCSVToSolr {
-	
-	@Resource(name = "wikidataService")
-	WikidataService wikidataService;
-	
-	@Resource
-	SolrWikidataEntityService solrWikidataEntityService;
-	
-	private String solrCore = "wikidata";
-	private String wikidataDirectory = "C:/wikidata";
-	
-	@Test
-	public void test() throws Exception {		
-
-		File directory=new File(wikidataDirectory);
-		File [] listFiles = directory.listFiles();
-	    int fileCount=listFiles.length;
-	    
-	    int solrDocumentsInOneFile = 300;
-	    int solrFileNumber = 1;
-	    int solrMaxDocumentsReached = solrFileNumber*solrDocumentsInOneFile;
-	    
-	    String fileNameSchema = "C:/wikidata_solr_xmls/wikidata_solr_";
-	    
-		
-	    //write to this file
-		String newOutpuFileName = fileNameSchema + String.valueOf("0") + "_" + String.valueOf(solrMaxDocumentsReached-1) + ".xml";		
-		BufferedWriter bw = new BufferedWriter(new FileWriter(new File(newOutpuFileName), true));
-	  
-		for(int i=0; i<fileCount; i++)
-		{
-			
-
-			
-			//read from this file
-			String contentJsonFile = null;
-			try {
-	            
-				contentJsonFile = FileUtils.readFileToString(listFiles[i], StandardCharsets.UTF_8);
-	            
-	        } catch (IOException e) {
-	        	
-				e.printStackTrace();
-	        }
-			
-			
-			DocumentObjectBinder binder = new DocumentObjectBinder();
-			
-			String fileName=listFiles[i].getName();
-			
-			String wikidataIdentifierWithJsonExtension = fileName.substring(fileName.lastIndexOf("-") + 1);
-			
-			String wikidataURL = "http://www.wikidata.org/entity/"+wikidataIdentifierWithJsonExtension.substring(0,wikidataIdentifierWithJsonExtension.length()-5);
-			
-			WikidataEntity wikiEntity = wikidataService.getWikidataEntity(wikidataURL, contentJsonFile, "place");
-			
-			String xml = null;
-			if(wikiEntity instanceof WikidataAgent)
-			{
-				WikidataAgent agentLocal = (WikidataAgent) wikiEntity;
-				SolrWikidataAgentImpl solrWikidataAgent = null;
-				
-				if(agentLocal instanceof SolrWikidataAgentImpl) {
-					solrWikidataAgent=(SolrWikidataAgentImpl) agentLocal;
-				}
-				else {
-					solrWikidataAgent=new SolrWikidataAgentImpl(agentLocal);
-				}
-				
-				xml = ClientUtils.toXML(binder.toSolrInputDocument(solrWikidataAgent));
-				
-			}
-			else if (wikiEntity instanceof WikidataPlace)
-			{
-				WikidataPlace placeLocal = (WikidataPlace) wikiEntity;
-				SolrWikidataPlaceImpl solrWikidataPlace = null;		
-				
-				if(placeLocal instanceof SolrWikidataPlaceImpl) {
-					solrWikidataPlace=(SolrWikidataPlaceImpl) placeLocal;
-				}
-				else {
-					solrWikidataPlace=new SolrWikidataPlaceImpl(placeLocal);
-				}
-				
-				xml = ClientUtils.toXML(binder.toSolrInputDocument(solrWikidataPlace));
-			
-			}
-			
-			if(i>=solrMaxDocumentsReached)
-			{
-				
-				//close old file and write to a new file
-				bw.close();
-				
-				newOutpuFileName = fileNameSchema + String.valueOf(solrMaxDocumentsReached) + "_" + String.valueOf(solrMaxDocumentsReached+solrDocumentsInOneFile-1) + ".xml";
-				bw = new BufferedWriter(new FileWriter(new File(newOutpuFileName), true));
-				solrFileNumber+=1;
-				solrMaxDocumentsReached = solrFileNumber*solrDocumentsInOneFile;
-			    
-				try {	    	
-					
-					bw.append(xml + "\n");
-					
-				} catch (IOException ioe) 
-			    {
-					ioe.printStackTrace();
-			    }
-			}
-			else
-			{
-				try {	    	
-					
-					bw.append(xml + "\n");
-					
-				} catch (IOException ioe) 
-			    {
-					ioe.printStackTrace();
-			    }
-			}
-			
-			
-			
-			
-			
-		}
-		
-		assertTrue(true);
-		
-	}
-
-}
+//package eu.europeana.enrichment.utils;
+//
+//import static org.junit.Assert.assertTrue;
+//
+//import java.io.BufferedWriter;
+//import java.io.File;
+//import java.io.FileWriter;
+//import java.io.IOException;
+//import java.nio.charset.StandardCharsets;
+//
+//import javax.annotation.Resource;
+//
+//import org.apache.commons.io.FileUtils;
+//import org.apache.solr.client.solrj.beans.DocumentObjectBinder;
+//import org.apache.solr.client.solrj.util.ClientUtils;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//import eu.europeana.enrichment.model.WikidataAgent;
+//import eu.europeana.enrichment.model.WikidataEntity;
+//import eu.europeana.enrichment.model.WikidataPlace;
+//import eu.europeana.enrichment.ner.linking.WikidataService;
+//import eu.europeana.enrichment.solr.model.SolrWikidataAgentImpl;
+//import eu.europeana.enrichment.solr.model.SolrWikidataPlaceImpl;
+//import eu.europeana.enrichment.solr.service.SolrWikidataEntityService;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = "classpath:test-ner-config-book-dumitru.xml")
+//
+///**
+// * 
+// * @author StevaneticS
+// *
+// * Importing stories and items to the mongo db from a json file
+// */
+//
+//public class ImportPlacesFromCSVToSolr {
+//	
+//	@Resource(name = "wikidataService")
+//	WikidataService wikidataService;
+//	
+//	@Resource
+//	SolrWikidataEntityService solrWikidataEntityService;
+//	
+//	private String solrCore = "wikidata";
+//	private String wikidataDirectory = "C:/wikidata";
+//	
+//	@Test
+//	public void test() throws Exception {		
+//
+//		File directory=new File(wikidataDirectory);
+//		File [] listFiles = directory.listFiles();
+//	    int fileCount=listFiles.length;
+//	    
+//	    int solrDocumentsInOneFile = 300;
+//	    int solrFileNumber = 1;
+//	    int solrMaxDocumentsReached = solrFileNumber*solrDocumentsInOneFile;
+//	    
+//	    String fileNameSchema = "C:/wikidata_solr_xmls/wikidata_solr_";
+//	    	    
+//	    //write analyzed wikidata ids to this file in case an analysis needs to continue
+//	    String analysedWikidata = "C:/wikidata_solr_xmls/analysed_wikidata.txt";
+//	    File analysedWikidataFile=new File(analysedWikidata);
+//		//read from this file
+//		String contentAnalysedWikidata = null;
+//
+//		contentAnalysedWikidata = FileUtils.readFileToString(analysedWikidataFile, StandardCharsets.UTF_8);
+//            
+//
+//		
+//		BufferedWriter bwAnalysedWikidata = new BufferedWriter(new FileWriter(new File(analysedWikidata), true));
+//
+//		/*
+//		 * write not found wikidata entities where there is som eocntent in the .json file but that content
+//		 * is not the actual json but the info message that the data has not been found 
+//		 */
+//		String notFoundWikidata = "C:/wikidata_solr_xmls/not_found_wikidata.txt";
+//	    BufferedWriter bwNotFoundWikidata = new BufferedWriter(new FileWriter(new File(notFoundWikidata), true));
+//		
+//	    //write Solr xml documents to this file
+//		String newOutpuFileName = fileNameSchema + String.valueOf("0") + "_" + String.valueOf(solrMaxDocumentsReached-1) + ".xml";		
+//		BufferedWriter bw = new BufferedWriter(new FileWriter(new File(newOutpuFileName), true));
+//		bw.append("<add>" + "\n");
+//
+//	  
+//		for(int i=0; i<fileCount; i++)
+//		{
+//			
+//			if(!contentAnalysedWikidata.contains(listFiles[i].getName()))
+//			{
+//					
+//				System.out.print("Current analyzed file is: " + listFiles[i].getName() + " .\n");
+//				
+//				//read from this file
+//				String contentJsonFile = null;
+//				contentJsonFile = FileUtils.readFileToString(listFiles[i], StandardCharsets.UTF_8);
+//
+//				//adding file name to the list of analyzed files
+//
+//				bwAnalysedWikidata.append(listFiles[i].getName() + ",");
+//				System.out.print("Current wikidata entity is successfully added to the list of analyzed files. \n");
+//	
+//				
+//				//this file contains no real wikidata, but the message that the wikidata id is not found
+//				if(contentJsonFile.substring(0, 1).compareToIgnoreCase("{")!=0)
+//				{
+//
+//					bwNotFoundWikidata.append(listFiles[i].getName() + ",");
+//					System.out.print("Current wikidata entity contains no json. \n");
+//						
+//					continue;
+//				}
+//				
+//				
+//				DocumentObjectBinder binder = new DocumentObjectBinder();
+//				
+//				String fileName=listFiles[i].getName();
+//				
+//				String wikidataIdentifierWithJsonExtension = fileName.substring(fileName.lastIndexOf("-") + 1);
+//				
+//				String wikidataURL = "http://www.wikidata.org/entity/"+wikidataIdentifierWithJsonExtension.substring(0,wikidataIdentifierWithJsonExtension.length()-5);
+//				
+//				WikidataEntity wikiEntity = wikidataService.getWikidataEntity(wikidataURL, contentJsonFile, "place");
+//				
+//				System.out.print("Current wikidata entity is successfully created. \n");
+//				
+//				String xml = null;
+//				if(wikiEntity instanceof WikidataAgent)
+//				{
+//					WikidataAgent agentLocal = (WikidataAgent) wikiEntity;
+//					SolrWikidataAgentImpl solrWikidataAgent = null;
+//					
+//					if(agentLocal instanceof SolrWikidataAgentImpl) {
+//						solrWikidataAgent=(SolrWikidataAgentImpl) agentLocal;
+//					}
+//					else {
+//						solrWikidataAgent=new SolrWikidataAgentImpl(agentLocal);
+//					}
+//					
+//					xml = ClientUtils.toXML(binder.toSolrInputDocument(solrWikidataAgent));
+//					
+//				}
+//				else if (wikiEntity instanceof WikidataPlace)
+//				{
+//					WikidataPlace placeLocal = (WikidataPlace) wikiEntity;
+//					SolrWikidataPlaceImpl solrWikidataPlace = null;		
+//					
+//					if(placeLocal instanceof SolrWikidataPlaceImpl) {
+//						solrWikidataPlace=(SolrWikidataPlaceImpl) placeLocal;
+//					}
+//					else {
+//						solrWikidataPlace=new SolrWikidataPlaceImpl(placeLocal);
+//					}
+//					
+//					xml = ClientUtils.toXML(binder.toSolrInputDocument(solrWikidataPlace));
+//				
+//				}
+//				
+//				if(i>=solrMaxDocumentsReached)
+//				{
+//					
+//					int moduo = i % solrDocumentsInOneFile;
+//					solrMaxDocumentsReached = i - moduo;
+//					
+//					
+//					//append </add> and close old file and write to a new file
+//					bw.append("</add>" + "\n");
+//					bw.close();
+//					
+//					newOutpuFileName = fileNameSchema + String.valueOf(solrMaxDocumentsReached) + "_" + String.valueOf(solrMaxDocumentsReached+solrDocumentsInOneFile-1) + ".xml";
+//					bw = new BufferedWriter(new FileWriter(new File(newOutpuFileName), true));
+//					
+//					solrMaxDocumentsReached = solrMaxDocumentsReached + solrDocumentsInOneFile;
+//
+//					bw.append("<add>" + "\n");
+//					bw.append(xml + "\n");
+//						
+//					System.out.print("Current wikidata entity is successfully added to the new solr docs file. \n");
+//					
+//				}
+//				else
+//				{
+//					
+//					bw.append(xml + "\n");
+//						
+//					System.out.print("Current wikidata entity is successfully added to the existing solr docs file. \n");
+//					
+//				}
+//				
+//				
+//				
+//			}
+//		
+//			
+//		}
+//		
+//		bwNotFoundWikidata.close();
+//		bwAnalysedWikidata.close();
+//		bw.close();
+//		assertTrue(true);
+//		
+//	}
+//
+//}
