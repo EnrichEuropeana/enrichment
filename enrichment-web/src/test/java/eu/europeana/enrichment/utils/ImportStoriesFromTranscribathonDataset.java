f452f3e68623d3ae0dae47edb5b4b46bb590e603
GordeaS <4517853+gsergiu@users.noreply.github.com>
2024-10-06 21:29:20 +0200

    remove dependency of old TP definitions library

--- enrichment-web/src/test/java/eu/europeana/enrichment/utils/ImportStoriesFromTranscribathonDataset.java
+++ enrichment-web/src/test/java/eu/europeana/enrichment/utils/ImportStoriesFromTranscribathonDataset.java
@@ -3,7 +3,6 @@ package eu.europeana.enrichment.utils;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
-import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,100 +11,105 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import eu.europeana.enrichment.common.commons.HelperFunctions;
-import eu.europeana.enrichment.definitions.model.impl.StoryEntityImpl;
 import eu.europeana.enrichment.mongo.service.PersistentItemEntityService;
 import eu.europeana.enrichment.mongo.service.PersistentStoryEntityService;
 import eu.europeana.enrichment.mongo.service.PersistentTranslationEntityService;
 import eu.europeana.enrichment.solr.commons.JavaGsonJSONParser;
-import objects.Dataset;
-import objects.Item;
-import objects.Story;
-import objects.Transcription;
 
 /**
-* 
-* @author StevaneticS
-*
-* Importing stories and items to the mongo db from a json file
-*/
+ * 
+ * @author StevaneticS
+ *
+ *         Importing stories and items to the mongo db from a json file
+ */
 //@ContextConfiguration(classes = { EnrichmentApp.class})
 @SpringBootTest
 @Disabled("Excluded from automated runs.")
 public class ImportStoriesFromTranscribathonDataset {
-		
-	Logger logger = LogManager.getLogger(getClass());
-	
-	@Autowired
-	PersistentStoryEntityService persistentStoryEntityService;
-	@Autowired
-	PersistentItemEntityService persistentItemEntityService;
-	@Autowired
-	PersistentTranslationEntityService persistentTranslationEntityService;
-	@Autowired
-	JavaGsonJSONParser javaJSONParser;
-	
-	@Test
-	public void getDatasetEuropeana1989FromTranscribathon() throws IOException {
-		String datasetId = "";
-		String datasetName = "Europeana 1989";
-		String URLDataset = "https://europeana.fresenia.man.poznan.pl/tp-api/datasets/";
-		String URLStoriesMinimal = "https://europeana.fresenia.man.poznan.pl/tp-api/storiesMinimal?DatasetId=";
-		String URLStoriesComplete = "https://europeana.fresenia.man.poznan.pl/tp-api/stories/";
-		String URLItemTranscription = "https://europeana.fresenia.man.poznan.pl/tp-api/transcriptions?ItemId=";
-		String responseDatasets = HelperFunctions.createHttpRequest(null, URLDataset, null);
-		assertNotNull(responseDatasets);
-		
-		ObjectMapper objectMapper = new ObjectMapper();
-		List<Dataset> listDatasets = objectMapper.readValue(responseDatasets, new TypeReference<List<Dataset>>(){});
-		assertNotNull(listDatasets);
 
-		for(Dataset dataset : listDatasets) {
-        	if(dataset.Name.equals(datasetName)) {
-            	datasetId=dataset.DatasetId.toString();
-            	break;
-            }
-        }
-		
-		String responseStoriesMinimal = HelperFunctions.createHttpRequest(null, URLStoriesMinimal+datasetId, null);
+    Logger logger = LogManager.getLogger(getClass());
 
-		List<Story> listStoriesMinimal = objectMapper.readValue(responseStoriesMinimal, new TypeReference<List<Story>>(){});
-		if(listStoriesMinimal==null || listStoriesMinimal.isEmpty()) return;
-		
-		for (Story minimalStory : listStoriesMinimal) {
+    @Autowired
+    PersistentStoryEntityService persistentStoryEntityService;
+    @Autowired
+    PersistentItemEntityService persistentItemEntityService;
+    @Autowired
+    PersistentTranslationEntityService persistentTranslationEntityService;
+    @Autowired
+    JavaGsonJSONParser javaJSONParser;
 
-			String responseStoriesComplete = HelperFunctions.createHttpRequest(null, URLStoriesComplete+minimalStory.StoryId.toString(), null);
+    @Test
+    public void getDatasetEuropeana1989FromTranscribathon() throws IOException {
+        String datasetId = "";
+        String datasetName = "Europeana 1989";
+        String URLDataset = "https://europeana.fresenia.man.poznan.pl/tp-api/datasets/";
+        String URLStoriesMinimal = "https://europeana.fresenia.man.poznan.pl/tp-api/storiesMinimal?DatasetId=";
+        String URLStoriesComplete = "https://europeana.fresenia.man.poznan.pl/tp-api/stories/";
+        String URLItemTranscription = "https://europeana.fresenia.man.poznan.pl/tp-api/transcriptions?ItemId=";
+        String responseDatasets = HelperFunctions.createHttpRequest(null, URLDataset, null);
+        assertNotNull(responseDatasets);
 
-			List<Story> listStoriesComplete = objectMapper.readValue(responseStoriesComplete, new TypeReference<List<Story>>(){});
+        
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        List<Dataset> listDatasets = objectMapper.readValue(responseDatasets, new TypeReference<List<Dataset>>() {
+//        });
+//        assertNotNull(listDatasets);
+//
+//        for (Dataset dataset : listDatasets) {
+//            if (dataset.Name.equals(datasetName)) {
+//                datasetId = dataset.DatasetId.toString();
+//                break;
+//            }
+//        }
+//
+//        String responseStoriesMinimal = HelperFunctions.createHttpRequest(null, URLStoriesMinimal + datasetId, null);
+//
+//        List<Story> listStoriesMinimal = objectMapper.readValue(responseStoriesMinimal,
+//                new TypeReference<List<Story>>() {
+//                });
+//        if (listStoriesMinimal == null || listStoriesMinimal.isEmpty())
+//            return;
+//
+//        for (Story minimalStory : listStoriesMinimal) {
+//
+//            String responseStoriesComplete = HelperFunctions.createHttpRequest(null,
+//                    URLStoriesComplete + minimalStory.StoryId.toString(), null);
+//
+//            List<Story> listStoriesComplete = objectMapper.readValue(responseStoriesComplete,
+//                    new TypeReference<List<Story>>() {
+//                    });
+//
+//            List<Item> storyItems = listStoriesComplete.get(0).Items;
+//            if (storyItems == null || storyItems.isEmpty())
+//                continue;
+//
+//            String storyTranscription = "";
+//            for (Item item : storyItems) {
+//
+//                String responseItemTranscription = HelperFunctions.createHttpRequest(null,
+//                        URLItemTranscription + item.ItemId.toString(), null);
+//                List<Transcription> listItemTranscription = objectMapper.readValue(responseItemTranscription,
+//                        new TypeReference<List<Transcription>>() {
+//                        });
+//                if (listItemTranscription != null && listItemTranscription.size() > 0) {
+//                    storyTranscription += listItemTranscription.get(0).Text;
+//                }
+//            }
+//
+//            StoryEntityImpl[] newStories = new StoryEntityImpl[1];
+//            newStories[0] = new StoryEntityImpl();
+//
+//            if (!storyTranscription.equalsIgnoreCase(""))
+//                newStories[0].setTranscriptionText(storyTranscription);
+//            newStories[0].setDescription(minimalStory.dcDescription);
+//            newStories[0].setSource(minimalStory.dcSource);
+//            newStories[0].setStoryId(minimalStory.StoryId.toString());
+//            newStories[0].setTitle(minimalStory.dcTitle);
 
-			List<Item> storyItems = listStoriesComplete.get(0).Items;
-			if(storyItems==null || storyItems.isEmpty()) continue;
-			
-			String storyTranscription = "";
-			for(Item item : storyItems) {
-				
-				String responseItemTranscription = HelperFunctions.createHttpRequest(null, URLItemTranscription+item.ItemId.toString(), null);
-				List<Transcription> listItemTranscription = objectMapper.readValue(responseItemTranscription, new TypeReference<List<Transcription>>(){});
-				if(listItemTranscription!=null && listItemTranscription.size()>0) {
-					storyTranscription+=listItemTranscription.get(0).Text;
-				}
-			}
-			
-			StoryEntityImpl [] newStories = new StoryEntityImpl [1];
-			newStories[0] = new StoryEntityImpl();
-			
-			if(!storyTranscription.equalsIgnoreCase("")) newStories[0].setTranscriptionText(storyTranscription);
-			newStories[0].setDescription(minimalStory.dcDescription);
-			newStories[0].setSource(minimalStory.dcSource);
-			newStories[0].setStoryId(minimalStory.StoryId.toString());
-			newStories[0].setTitle(minimalStory.dcTitle);
-			
-			//uploadStories(newStories);
+            // uploadStories(newStories);
 
-			//extracting items in one json file
+            // extracting items in one json file
 //			String fileName = "C:/conceptual_search_documents/allStoryTranslations-BERT-LDA.csv";
 //			BufferedWriter bwTranslations = new BufferedWriter(new FileWriter(new File(fileName)));
 //			bwTranslations.write("storyId,text"+"\n");				
@@ -128,7 +132,7 @@ public class ImportStoriesFromTranscribathonDataset {
 //			}		
 //			bwTranslations.close();
 
-			}
-		}
+//        }
+    }
 
 }
